34f4f03946da985ca9b850452c11e4fcd1153488
Mo Mansour <mohmans@pm.me>
2026-06-15 15:44:24 +0300

    fix: corner marks

--- app/components/CornerMarks.vue
+++ app/components/CornerMarks.vue
@@ -3,20 +3,22 @@ withDefaults(defineProps<{ size?: number }>(), { size: 10 })
 </script>
 
 <template>
-  <span
-    class="absolute opacity-30 border-(--bp-accent)"
-    :style="{ width: `${size}px`, height: `${size}px`, top: '-1px', insetInlineStart: '-1px', borderBlockStart: '2px solid', borderInlineStart: '2px solid' }"
-  />
-  <span
-    class="absolute opacity-30 border-(--bp-accent)"
-    :style="{ width: `${size}px`, height: `${size}px`, top: '-1px', insetInlineEnd: '-1px', borderBlockStart: '2px solid', borderInlineEnd: '2px solid' }"
-  />
-  <span
-    class="absolute opacity-30 border-(--bp-accent)"
-    :style="{ width: `${size}px`, height: `${size}px`, bottom: '-1px', insetInlineStart: '-1px', borderBlockEnd: '2px solid', borderInlineStart: '2px solid' }"
-  />
-  <span
-    class="absolute opacity-30 border-(--bp-accent)"
-    :style="{ width: `${size}px`, height: `${size}px`, bottom: '-1px', insetInlineEnd: '-1px', borderBlockEnd: '2px solid', borderInlineEnd: '2px solid' }"
-  />
+  <div class="absolute inset-0 pointer-events-none">
+    <span
+      class="absolute top-[-1px] start-[-1px] opacity-30 border-t-2 border-s-2 border-(--bp-accent)"
+      :style="{ width: `${size}px`, height: `${size}px` }"
+    />
+    <span
+      class="absolute top-[-1px] end-[-1px] opacity-30 border-t-2 border-e-2 border-(--bp-accent)"
+      :style="{ width: `${size}px`, height: `${size}px` }"
+    />
+    <span
+      class="absolute bottom-[-1px] start-[-1px] opacity-30 border-b-2 border-s-2 border-(--bp-accent)"
+      :style="{ width: `${size}px`, height: `${size}px` }"
+    />
+    <span
+      class="absolute bottom-[-1px] end-[-1px] opacity-30 border-b-2 border-e-2 border-(--bp-accent)"
+      :style="{ width: `${size}px`, height: `${size}px` }"
+    />
+  </div>
 </template>
